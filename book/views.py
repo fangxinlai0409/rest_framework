258a1c382b8e16dd72bab9ca8e1bb78f04614df7
fangxinlai0409 <fxfasdf1234@163.com>
2024-09-23 11:22:52 +0000

    init

---
+++ book/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import View
+from book.models import BookInfo
+from django.http import JsonResponse
+import json
+
+
+
+
+
+
+# Create your views here.
+class BookListView(View):
+    """
+    查询所有图书、增加图书
+    """
+    def get(self, request):
+        """
+        查询所有图书
+        路由：GET /books/
+        """
+        queryset = BookInfo.objects.all()
+        book_list = []
+        for book in queryset:
+            book_list.append({
+                'id': book.id,
+                'name': book.name,
+                'pub_date': book.pub_date
+            })
+        return JsonResponse(book_list, safe=False)
+
+    def post(self, request):
+        """
+        新增图书
+        路由：POST /books/
+        """
+        json_bytes = request.body
+        json_str = json_bytes.decode()
+        book_dict = json.loads(json_str)
+
+        # 此处详细的校验参数省略
+
+        book = BookInfo.objects.create(
+            name=book_dict.get('name'),
+            pub_date=book_dict.get('pub_date')
+        )
+
+        return JsonResponse({
+            'id': book.id,
+            'name': book.name,
+            'pub_date': book.pub_date
+        },safe=False)
+
+class BookDetailView(View):
+    """
+    获取单个图书信息
+    修改图书信息
+    删除图书
+    """
+    def get(self, request, pk):
+        """
+        获取单个图书信息
+        路由： GET  /books/<pk>/
+        """
+        try:
+            book = BookInfo.objects.get(id=pk)
+        except BookInfo.DoesNotExist:
+            return JsonResponse({},status=404)
+
+        return JsonResponse({
+            'id': book.id,
+            'name': book.name,
+            'pub_date': book.pub_date
+        })
+
+    def put(self, request, pk):
+        """
+        修改图书信息
+        路由： PUT  /books/<pk>
+        """
+        try:
+            book = BookInfo.objects.get(id=pk)
+        except BookInfo.DoesNotExist:
+            return JsonResponse({},status=404)
+
+        json_bytes = request.body
+        json_str = json_bytes.decode()
+        book_dict = json.loads(json_str)
+
+        # 此处详细的校验参数省略
+
+        book.name = book_dict.get('name')
+        book.pub_date = book_dict.get('pub_date')
+        book.save()
+
+        return JsonResponse({
+            'id': book.id,
+            'name': book.name,
+            'pub_date': book.pub_date
+        })
+
+    def delete(self, request, pk):
+        """
+        删除图书
+        路由： DELETE /books/<pk>/
+        """
+        try:
+            book = BookInfo.objects.get(id=pk)
+        except BookInfo.DoesNotExist:
+            return JsonResponse({},status=404)
+
+        book.delete()
+
+        return JsonResponse({},status=204)
