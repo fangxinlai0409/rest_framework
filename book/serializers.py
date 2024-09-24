6f7b0e0d5db8e26778a3828cb99499d54d2210ec
fangxinlai0409 <fxfasdf1234@163.com>
2024-09-24 05:10:09 +0000

    anti serializers defined

--- book/serializers.py
+++ book/serializers.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
+
+from book.models import BookInfo
+
+
 class BookInfoSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
-    pub_data = serializers.DateField()
+    pub_date = serializers.DateField()
     readcount = serializers.IntegerField()
+
+
+class PeopleInfoSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    password = serializers.CharField()
+    description = serializers.CharField()
+    is_delete=serializers.BooleanField()
+    # book_id=serializers.IntegerField()
